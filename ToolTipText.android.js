6130c9e91988b2a00e42689bbd47af8ffc48bf21
chirag04 <jain_chirag04@yahoo.com>
2015-05-18 23:34:14 -0400

    add android warning

---
+++ ToolTipText.android.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var warning = require('warning');
+
+var ToolTipText = {
+  test: function() {
+    warning("Not yet implemented for Android.");
+  }
+};
+
+module.exports = ToolTipText;
