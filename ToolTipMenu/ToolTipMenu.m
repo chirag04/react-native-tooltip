421efad2495f4f0b5b639fba46076b0df5a1b893
chirag04 <jain_chirag04@yahoo.com>
2015-09-23 18:11:51 -0400

    exec on main thread

--- ToolTipMenu/ToolTipMenu.m
+++ ToolTipMenu/ToolTipMenu.m
@@ -13,35 +13,27 @@ RCT_EXPORT_MODULE()
 
 - (dispatch_queue_t)methodQueue
 {
-    return _bridge.uiManager.methodQueue;
+    return dispatch_get_main_queue();
 }
 
 RCT_EXPORT_METHOD(show:(nonnull NSNumber *)reactTag
                   items: (NSArray *)items)
 {
-    [_bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, RCTSparseArray *viewRegistry) {
-        
-        RCTToolTipText *view = viewRegistry[reactTag];
-        if (!view) {
-            RCTLogError(@"Cannot find view with tag #%@", reactTag);
-            return;
-        }
-        
-        NSArray *buttons = items;
-        NSMutableArray *menuItems = [NSMutableArray array];
-        for (NSString *buttonText in buttons) {
-            NSString *sel = [NSString stringWithFormat:@"magic_%@", buttonText];
-            [menuItems addObject:[[UIMenuItem alloc]
-                                  initWithTitle:buttonText
-                                  action:NSSelectorFromString(sel)]];
-        }
-        [view becomeFirstResponder];
-        UIMenuController *menuCont = [UIMenuController sharedMenuController];
-        [menuCont setTargetRect:view.frame inView:view.superview];
-        menuCont.arrowDirection = UIMenuControllerArrowDown;
-        menuCont.menuItems = menuItems;
-        [menuCont setMenuVisible:YES animated:YES];
-    }];
+    UIView *view = [self.bridge.uiManager viewForReactTag:reactTag];
+    NSArray *buttons = items;
+    NSMutableArray *menuItems = [NSMutableArray array];
+    for (NSString *buttonText in buttons) {
+        NSString *sel = [NSString stringWithFormat:@"magic_%@", buttonText];
+        [menuItems addObject:[[UIMenuItem alloc]
+                              initWithTitle:buttonText
+                              action:NSSelectorFromString(sel)]];
+    }
+    [view becomeFirstResponder];
+    UIMenuController *menuCont = [UIMenuController sharedMenuController];
+    [menuCont setTargetRect:view.frame inView:view.superview];
+    menuCont.arrowDirection = UIMenuControllerArrowDown;
+    menuCont.menuItems = menuItems;
+    [menuCont setMenuVisible:YES animated:YES];
 }
 
 @end
