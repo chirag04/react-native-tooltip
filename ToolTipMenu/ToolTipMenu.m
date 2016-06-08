987583b00be723489be4e0fd59f17b81a6221e99
Arthur Wang <arthraim@gmail.com>
2016-06-08 18:56:05 +0800

    introduce a new interface

--- ToolTipMenu/ToolTipMenu.m
+++ ToolTipMenu/ToolTipMenu.m
@@ -35,4 +35,28 @@ RCT_EXPORT_METHOD(show:(nonnull NSNumber *)reactTag
     [menuCont setMenuVisible:YES animated:YES];
 }
 
+RCT_EXPORT_METHOD(showFromRect:(nonnull NSNumber *)reactTag
+                  items: (NSArray *)items
+                  rect: (NSArray *)rectArr)
+{
+    CGRect rect = CGRectMake([rectArr[0] floatValue], [rectArr[1] floatValue],
+                             [rectArr[2] floatValue], [rectArr[3] floatValue]);
+    UIView *view = [self.bridge.uiManager viewForReactTag:reactTag];
+    NSArray *buttons = items;
+    NSMutableArray *menuItems = [NSMutableArray array];
+    for (NSString *buttonText in buttons) {
+        NSString *sel = [NSString stringWithFormat:@"magic_%@", buttonText];
+        [menuItems addObject:[[UIMenuItem alloc]
+                              initWithTitle:buttonText
+                              action:NSSelectorFromString(sel)]];
+    }
+    [view becomeFirstResponder];
+    UIMenuController *menuCont = [UIMenuController sharedMenuController];
+    view.superview.frame = rect;
+    [menuCont setTargetRect:view.frame inView:view.superview];
+    menuCont.arrowDirection = UIMenuControllerArrowDown;
+    menuCont.menuItems = menuItems;
+    [menuCont setMenuVisible:YES animated:YES];
+}
+
 @end
